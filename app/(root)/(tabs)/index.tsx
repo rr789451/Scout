fec9a36f5546acd8157873094188f56c0a812d99
Rohit <rr789451@gmail.com>
2025-03-30 02:09:55 +0530

    feat: Milestone 2 - Implemented Home screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,24 +1,61 @@
 import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
 import SearchBar from "@/components/SearchBar";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
 import { useGlobalContext } from "@/lib/global-provider";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import seed from "@/lib/seed";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext();
+  const params = useLocalSearchParams<{ query?: string; filter?: string; }>()
+
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({
+    fn: getLatestProperties
+  })
+
+  const { data: properties, loading, refetch } = useAppwrite({
+    fn: getProperties,
+    params:{
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    },
+    skip: true,
+  })
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`)
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    })
+  }, [params.filter, params.query])
+
   return (
     <SafeAreaView className="bg-white h-full">
       <FlatList 
-        data={[1, 2, 3, 4]}
-        renderItem={({item}) => <Card />}
+        data={properties}
+        renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
         keyExtractor={(item) => item.toString()}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : <NoResults />
+        }
         ListHeaderComponent= {
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
@@ -45,15 +82,22 @@ export default function Index() {
                   <Text className="text-base font-rubik-bold text-primary-300">See More</Text>
                 </TouchableOpacity>
               </View>
-              <FlatList 
-                data={[1, 2, 3, 4]}
-                renderItem={({item}) => <FeaturedCard />}
-                keyExtractor={(item) => item.toString()}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                bounces={false}
-                contentContainerClassName="flex gap-5 mt-5"
-              />
+              {latestPropertiesLoading ?
+                <ActivityIndicator size="large" className="text-primary-300" /> : 
+                !latestProperties || latestProperties.length === 0 ? 
+                <NoResults /> : 
+                (
+                  <FlatList 
+                    data={latestProperties}
+                    renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
+                    keyExtractor={(item) => item.toString()}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    bounces={false}
+                    contentContainerClassName="flex gap-5 mt-5"
+                  />
+                )
+              }
             </View>
             <View className="flex flex-row items-center justify-between">
               <Text className="text-xl font-rubik-bold text-black-300">Our Recommendations</Text>

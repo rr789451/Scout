5c39287cfa2154ecdd856e5997867dfd0977b381
Rohit <rr789451@gmail.com>
2025-03-30 02:31:25 +0530

    feat: Milestone 3 - Implemented Explore screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -46,7 +46,7 @@ export default function Index() {
       <FlatList 
         data={properties}
         renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
-        keyExtractor={(item) => item.toString()}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
@@ -90,7 +90,7 @@ export default function Index() {
                   <FlatList 
                     data={latestProperties}
                     renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
-                    keyExtractor={(item) => item.toString()}
+                    keyExtractor={(item) => item.$id}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     bounces={false}
